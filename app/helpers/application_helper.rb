9f831d41f22f61967bc50efd39fbd8ea59658837
Philip Brocoum <philip.brocoum@gmail.com>
2018-04-11 23:10:15 -0700

    retina.js v 2, image_2x helper, cover html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def image_2x source, options={}
+    source_2x = File.basename(source, File.extname(source)) + "@2x" + File.extname(source)
+    path_2x = image_path source_2x
+    image_tag source, options.merge({"data-rjs": path_2x})
+  end
 end
