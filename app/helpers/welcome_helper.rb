80d50c56be882553ce12f1a954dab3487d9059f2
Philip Brocoum <philip.brocoum@gmail.com>
2018-04-13 14:26:18 -0700

    Adding career section and displaying sites as cards

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,4 +2,14 @@ module WelcomeHelper
   def link_to_social fa, source, options={}
     link_to "<i class='fab fa-#{fa} fa-lg fa-fw'></i>".html_safe, source, options.merge({class: "btn btn-lg btn-outline-light p-2"})
   end
+
+  def pretty_site_link site
+    host = URI(site.link).host
+    if host == "web.archive.org"
+      host = site.name.downcase
+    end
+    host = "www.#{host}" unless host.include?("www.")
+    host = "#{host}.com" unless host.include?(".com")
+    return host
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :person
   scope :skills, -> { where type_of: "skill" }
+  scope :sites, -> { where type_of: "site" }
 end
