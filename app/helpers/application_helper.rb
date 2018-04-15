5ba77ea74583cecd2676654fc98fe499c74b7398
Philip Brocoum <philip.brocoum@gmail.com>
2018-04-14 21:40:12 -0700

    Cleaning up cover page and making background image extend to edge of browser

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
+  def filename_append source, append
+    File.basename(source, File.extname(source)) + append + File.extname(source)
+  end
+
   def image_2x source, options={}
-    source_2x = File.basename(source, File.extname(source)) + "@2x" + File.extname(source)
+    source_2x = filename_append(source, "@2x")
     path_2x = image_path source_2x
     image_tag source, options.merge({"data-rjs": path_2x})
   end
