81767afba44aa67985f39ff3e60d7433f0507c36
Philip Brocoum <philip.brocoum@gmail.com>
2018-04-18 09:07:19 -0700

    Using display: inline-flex; to vertical align icons

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,6 +1,6 @@
 module WelcomeHelper
   def link_to_social fa, source, options={}
-    link_to "<svg class=\"icon icon-#{fa}\"><use xlink:href=\"#icon-#{fa}\"></use></svg>".html_safe, source, options.merge({class: "btn btn-lg btn-outline-light p-2"})
+    link_to "<svg class=\"icon icon-#{fa}\"><use xlink:href=\"#icon-#{fa}\"></use></svg>".html_safe, source, options.merge({class: "btn btn-lg btn-outline-light p-2 icon-container"})
   end
 
   def pretty_site_link site
