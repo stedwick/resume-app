2b74bbbaa941c1cbe6c84b9ba79e432d1bdf07b1
Philip Brocoum <philip.brocoum@gmail.com>
2018-04-19 09:19:49 -0700

    Caching!

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    # Caching
+    my_etag          = @person
+    my_last_modified = @person.updated_at.utc
+    my_expires_in    = 1.hour
+    my_expires_in    = 1.minute if Rails.env.staging?
+    my_expires_in    = 15.seconds if Rails.env.development?
+    fresh_when etag: my_etag, last_modified: my_last_modified, public: true
+    expires_in my_expires_in, public: true
+    request.session_options[:skip] = true
   end
 end
