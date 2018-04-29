4c96428685b46082fb2693a73a28861b9168db81
Philip Brocoum <philip.brocoum@gmail.com>
2018-04-28 17:25:01 -0700

    By default caching is disabled in dev

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -21,7 +21,14 @@ class WelcomeController < ApplicationController
     my_last_modified = @person.updated_at.utc
     my_expires_in    = 1.hour
     my_expires_in    = 1.minute if Rails.env.staging?
-    my_expires_in    = 15.seconds if Rails.env.development?
+    if Rails.env.development?
+      # Enable/disable caching. By default caching is disabled.
+      if Rails.root.join('tmp/caching-dev.txt').exist?
+        my_expires_in = 15.seconds
+      else
+        my_expires_in = 1.seconds
+      end
+    end
     fresh_when etag: my_etag, last_modified: my_last_modified, public: true
     expires_in my_expires_in, public: true
     request.session_options[:skip] = true
