c3764cd1acc3e6a0b3a73e8e081e976d84025ca6
Philip Brocoum <philip.brocoum@gmail.com>
2018-05-02 18:23:11 -0700

    resume and vcard

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,4 +6,13 @@ class Person < ApplicationRecord
   has_many :schools
   has_many :jobs
   has_many :interests
+
+  def resume_file
+    name.sub(' ', '_') + "_resume.pdf"
+  end
+
+  def vcard_file
+    name.sub(' ', '_') + "_vCard.vcf"
+  end
+  
 end
